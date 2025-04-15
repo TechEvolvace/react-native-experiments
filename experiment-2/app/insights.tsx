4df507f521147a11da1d5d2cde619d62b49967ef
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-04-14 23:43:40 -0500

    Added new Navigation buttons in Insights page!

--- experiment-2/app/insights.tsx
+++ experiment-2/app/insights.tsx
@@ -1,5 +1,6 @@
 // Insights page
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,7 +11,19 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Welcome to the Insights page! This page is where I record my findings when developing this app for this Experiment!</Text>
+        {/* Navigation Buttons */}
+        <View style={{flexDirection: "row"}}>
+            <Link href="/" asChild>
+                <Button title="Home" />
+            </Link>
+            <Link href="/about" asChild>
+                <Button title="About" />
+            </Link>
+        </View>
+
+        {/* Insights page content */}
+        <Text>Welcome to the Insights page!</Text>
+        <Text>This page is where I record my findings when developing this app for this Experiment!</Text>
     </View>
   );
 }
