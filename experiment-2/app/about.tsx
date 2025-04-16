48e45b7769312a1158fa566b42c7a1d06c7e71ed
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-04-15 23:11:40 -0500

    Added navigation buttons in the About page!

--- experiment-2/app/about.tsx
+++ experiment-2/app/about.tsx
@@ -1,5 +1,6 @@
 // About page
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,6 +11,20 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      {/* Navigation Buttons */}
+      <View style={{flexDirection: "row"}}>
+          <Link href="/" asChild>
+            <Button title="Home" />
+          </Link>
+          <Link href="/about" asChild>
+            <Button title="About" />
+          </Link>
+          <Link href="/insights" asChild>
+            <Button title="Insights" />
+          </Link>
+      </View>
+
+      {/* About page content */}
       <Text>Welcome to the About page! I will tell you about the purpose behind this app in this Experiment here!</Text>
     </View>
   );
