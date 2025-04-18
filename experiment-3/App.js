b49f9adf06fff924949e8638c5a4cba56575dac0
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-04-17 23:50:58 -0500

    Updated content of the React Native app's landing page to confirm successful run of new React Native app!

--- experiment-3/App.js
+++ experiment-3/App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Successfully created new React Native app that have starter code without TypeScript files!</Text>
+      <Text>This app is also compatible with Expo Go to be able to run this app on an iPhone device that has Expo Go app installed!</Text>
       <StatusBar style="auto" />
     </View>
   );
