cecb84fba2b132b4be3729de40569284661dba1f
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2025-04-17 23:54:43 -0500

    Successfully applied style from Stylesheet in App.js file to the content in the React Native app's landing page!

--- experiment-3/App.js
+++ experiment-3/App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-
+      <Text style={styles.success_message}>Open up App.js to start working on your app!</Text>
       <Text>Successfully created new React Native app that have starter code without TypeScript files!</Text>
       <Text>This app is also compatible with Expo Go to be able to run this app on an iPhone device that has Expo Go app installed!</Text>
       <StatusBar style="auto" />
@@ -20,4 +19,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  success_message: {
+    color: 'green',
+  }
 });
